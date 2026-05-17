d088d36389e92a24460c2c3af5e1cbbfc4c86c4f
R1Scripts <131856678+R1Scripts@users.noreply.github.com>
2026-05-17 02:39:09 -0500

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -6,15 +6,18 @@ Config.DefaultStyle = 'tactical'
 
 Config.DefaultColor = '#ff003c'
 
+-- Fuente personalizada para la NUI.
+-- Debe existir dentro de html/fonts/ y puede ser .ttf, .otf, .woff o .woff2.
+-- Si lo dejas en nil o '', usará Segoe UI.
+Config.FontFile = 'STRIGER.ttf'
+Config.FontFamily = 'R1CustomFont'
+
 Config.Positions = {
     ['top-left'] = true,
     ['top-center'] = true,
     ['top-right'] = true,
-
     ['center-left'] = true,
-    ['center'] = true,
     ['center-right'] = true,
-
     ['bottom-left'] = true,
     ['bottom-center'] = true,
     ['bottom-right'] = true
